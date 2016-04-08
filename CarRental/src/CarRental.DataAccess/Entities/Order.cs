6faf20050e20e67253ac6682bdf7dd390498573f
pauliussx <skucas.paulius@gmail.com>
2016-04-08 15:36:09 +0300

    Dadeta uzsakymo klase

---
+++ CarRental/src/CarRental.DataAccess/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Entities
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int AutoId { get; set; }
+        public DateTime OrderStart { get; set; }
+        public DateTime OrderEnd { get; set; }
+        public string RentPlace { get; set; }
+        public string RentReturn { get; set; }
+        public decimal DayPrice { get; set; }
+        public string DeliveryPrice { get; set; }
+        public string Commentd { get; set; }
+    }
+}
