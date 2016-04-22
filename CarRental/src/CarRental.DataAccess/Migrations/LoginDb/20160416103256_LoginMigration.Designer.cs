b321856936dfa5e9fa70d4b3545b4d73341684e8
pauliussx <skucas.paulius@gmail.com>
2016-04-23 01:18:18 +0300

    css pakeitimai

--- CarRental/src/CarRental.DataAccess/Migrations/LoginDb/20160416103256_LoginMigration.Designer.cs
+++ CarRental/src/CarRental.DataAccess/Migrations/LoginDb/20160416103256_LoginMigration.Designer.cs
@@ -8,9 +8,10 @@ using CarRental.DataAccess;
 namespace CarRental.Web.Migrations.LoginDb
 {
     [DbContext(typeof(LoginDbContext))]
-    partial class LoginDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160416103256_LoginMigration")]
+    partial class LoginMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
