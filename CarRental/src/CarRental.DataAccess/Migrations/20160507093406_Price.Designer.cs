f9cbcddad5b9cf2d12b53fab98b81d9f74741ac1
pauliussx <skucas.paulius@gmail.com>
2016-05-07 12:35:39 +0300

    Price DB migracija

---
+++ CarRental/src/CarRental.DataAccess/Migrations/20160507093406_Price.Designer.cs
@@ -0,0 +1,87 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using CarRental.DataAccess;
+
+namespace CarRental.Web.Migrations
+{
+    [DbContext(typeof(CarRentalDbContext))]
+    [Migration("20160507093406_Price")]
+    partial class Price
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("CarRental.DataAccess.Entities.Auto", b =>
+                {
+                    b.Property<int>("AutoId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("Activity");
+
+                    b.Property<string>("AudioCode");
+
+                    b.Property<DateTime>("CascoInsurenceEnd");
+
+                    b.Property<string>("Color");
+
+                    b.Property<string>("Comments");
+
+                    b.Property<string>("DocumentNumber");
+
+                    b.Property<float>("EngineCapicity");
+
+                    b.Property<string>("FuelType");
+
+                    b.Property<string>("Gearbox");
+
+                    b.Property<DateTime>("InsurenceEnd");
+
+                    b.Property<int>("MainBelt");
+
+                    b.Property<string>("Make");
+
+                    b.Property<int>("Milleage");
+
+                    b.Property<int>("OilChange");
+
+                    b.Property<string>("PlateNumber");
+
+                    b.Property<DateTime>("TAEnd");
+
+                    b.Property<int>("Year");
+
+                    b.HasKey("AutoId");
+                });
+
+            modelBuilder.Entity("CarRental.DataAccess.Entities.Order", b =>
+                {
+                    b.Property<int>("OrderId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("AutoId");
+
+                    b.Property<string>("Commentd");
+
+                    b.Property<decimal>("DayPrice");
+
+                    b.Property<string>("DeliveryPrice");
+
+                    b.Property<DateTime>("OrderEnd");
+
+                    b.Property<DateTime>("OrderStart");
+
+                    b.Property<string>("RentPlace");
+
+                    b.Property<string>("RentReturn");
+
+                    b.HasKey("OrderId");
+                });
+        }
+    }
+}
---
+++ CarRental/src/CarRental.Web/Migrations/20160507093406_Price.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+
+namespace CarRental.Web.Migrations
+{
+    public partial class Price : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
