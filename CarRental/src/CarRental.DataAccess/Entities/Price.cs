46169f7410dbf2db0ba0c68d99c636467bb1dd92
pauliussx <skucas.paulius@gmail.com>
2016-05-07 12:32:57 +0300

    Kainos klases dadejimas

---
+++ CarRental/src/CarRental.DataAccess/Entities/Price.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Entities
+{
+    public class Price
+    {
+        public int PriceId { get; set; }
+        public int AutoId { get; set; }
+        public decimal Value { get; set; }
+        public int day { get; set; }
+    }
+}
