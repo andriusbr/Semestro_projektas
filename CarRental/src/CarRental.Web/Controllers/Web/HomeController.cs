4e5640866449b3b9de42229fb7c9957f67cc8f41
Andrius <andriusbruzga@yahoo.com>
2016-05-09 22:37:54 +0300

    Automobilio atvaizdavimas, pradinio puslapio css patobulinimai (nebaigta)

--- CarRental/src/CarRental.Web/Controllers/Web/HomeController.cs
+++ CarRental/src/CarRental.Web/Controllers/Web/HomeController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using CarRental.Web.Models;
+using Microsoft.AspNet.Mvc;
 
 namespace CarRental.Web.Controllers.Web
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            Car model = new Car("Volkswagen", "Polo"); 
             return View();
         }
     }
---
+++ CarRental/src/CarRental.Web/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Web.Models
+{
+    public class Car
+    {
+        public Car(string Make, string Model)
+        {
+            this.Make = Make;
+            this.Model = Model;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        
+
+    }
+}
