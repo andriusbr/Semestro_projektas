640d78d1ff499baedd7e2c7faa32e5bc532ee470
Andrius <andriusbruzga@yahoo.com>
2016-05-10 16:22:12 +0300

    Automobiliu dinamiskas uzkrovimas, uzsakymo forma (nebaigta)

---
+++ CarRental/src/CarRental.Web/Models/OrderSubmit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Web.Models
+{
+    public class OrderSubmit
+    {
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([+]?[0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3,6})$", ErrorMessage = "Not a valid Phone number")]
+        [Display(Name = "Phone")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Pick Up Location")]
+        public string PickUp { get; set; }
+
+        [Required]
+        [Display(Name = "Drop Off Location")]
+        public string DropOff { get; set; }
+
+        [Display(Name = "Comments (Optional)")]
+        public string Comments { get; set; }
+    }
+}
