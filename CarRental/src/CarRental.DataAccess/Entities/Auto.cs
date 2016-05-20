d566a77e01806a204bcf622a8aeaf816ada19b4b
pauliussx <skucas.paulius@gmail.com>
2016-05-20 17:48:18 +0300

    Ištrynus automobilį, grįžtama į automobilių sąrašą

--- CarRental/src/CarRental.DataAccess/Entities/Auto.cs
+++ CarRental/src/CarRental.DataAccess/Entities/Auto.cs
@@ -6,8 +6,8 @@ namespace CarRental.DataAccess.Entities
     public class Auto
     {
         public int AutoId { get; set; }
-        public string Model { get; set; }
         public string Make { get; set; }
+        public string Model { get; set; }        
         public int Year { get; set; }
         public string PlateNumber { get; set; }
         public string Color { get; set; }
